9e2a47f2d41d4c59b8866ccfdcb4cff2a2f15949
Glorzo <thisaraeranga@gmail.com>
2022-04-03 22:30:54 -0500

    JAWS access

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(routes);
 
 
 //Turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
 
